80c9a3692384fdf692c9a4f4ed3e5ae77466e420
JoseLomero <joselomero0@gmail.com>
2025-08-17 18:05:05 +0200

    beyblade detail update

--- src/app/private/beyblade-detail/beyblade-detail.component.ts
+++ src/app/private/beyblade-detail/beyblade-detail.component.ts
@@ -1,48 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { BeybladesListStore } from '../../core/store/beyblades-list.store';
-import { Beyblade } from '../beyblade-list/beyblade';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { AuthStore } from '../../core/store/auth.store';
+import { Component, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BeybladesListStore } from "../../core/store/beyblades-list.store";
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
-    selector: 'app-beyblade-detail',
-    templateUrl: './beyblade-detail.component.html',
-    styleUrls: ['./beyblade-detail.component.scss'],
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatDividerModule, MatIconModule, MatProgressSpinnerModule, MatToolbarModule]
+  selector: "app-beyblade-detail",
+  templateUrl: "./beyblade-detail.component.html",
+  styleUrls: ["./beyblade-detail.component.scss"],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+  ],
 })
 export class BeybladeDetailComponent implements OnInit {
-    public beyblade?: Beyblade;
-    public error?: string;
-    private readonly route = inject(ActivatedRoute)
-    private readonly beybladesListStore = inject(BeybladesListStore)
-    private readonly router = inject(Router)
-    private readonly authStore = inject(AuthStore)
+  private readonly beybladesListStore = inject(BeybladesListStore);
+  private readonly route = inject(ActivatedRoute);
 
+  public readonly beyblade$ = this.beybladesListStore.beyblade$;
+  public readonly loading$ = this.beybladesListStore.beybladeLoading$;
+  public error: string | null = null;
 
-    public ngOnInit(): void {
-        // const key = this.route.snapshot.paramMap.get('key');
-        // if (key) {
-        //     this.beybladesListStore.getBeyblade(key).then((beyblade) => {
-        //         this.beyblade = beyblade;
-        //     }).catch((error) => {
-        //         this.error = error.message;
-        //     });
-        // }
-    }
+  public ngOnInit(): void {
+    const key = this.route.snapshot.paramMap.get("key");
+    this.loadBeyblade(key);
+  }
 
-    public goBack(): void {
-        this.router.navigate(['/beybladesList']);
-    }
-
-    public logout(): void {
-        this.authStore.logout();
-        this.router.navigate(['/login']);
-    }
+  public loadBeyblade(beyblade: any): void {
+    this.beybladesListStore.loadBeyblade(beyblade);
+  }
 }
